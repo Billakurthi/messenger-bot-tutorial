98448917a0f4e269121f633e96761e93d74421b1
Billakurthi <avinash.billakurthi@gmail.com>
2017-06-24 20:25:31 +0530

    added webhook

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-var http = require('http');
 
-var server = http.createServer(function(request, response) {
+'use strict'
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World initial app!");
+var express = require("express");
+var app = express();
+var request = require("request");
 
-});
+var path = require("path");
+var bodyParser = require('body-parser');
+
+//parse text using body parser
+ app.use(bodyParser.json());
+
+ app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+ app.get('/webhook/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/webhook.html'))
+})
+// var http = require('http');
+// var server = http.createServer(function(request, response) {
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World initial app!");
+
+// });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
+app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
