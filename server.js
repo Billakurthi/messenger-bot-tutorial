a52707e49fd4b8b1c38dea7da2a0841d27d9d59d
Billakurthi <avinash.billakurthi@gmail.com>
2017-06-24 21:50:04 +0530

    code commit

--- server.js
+++ server.js
@@ -1,50 +1,16 @@
-﻿'use strict'
-
-var express = require('express');
+﻿var express = require('express');
 var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/webhook', function(req, res) {
+    res.sendFile(path.join(__dirname + '/webhook.html'));
+});
+
+
 var port = process.env.port || 1337
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
-
-
-
-
-
-// var express = require("express");
-// var app = express();
-// var request = require("request");
-
-// var path = require("path");
-// var bodyParser = require('body-parser');
-
-// //parse text using body parser
-// app.use(bodyParser.json());
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'))
-// })
-
-// app.get('/webhook/', function (req, res) {
-//     res.sendFile(path.join(__dirname + '/webhook.html'))
-// })
-// // var http = require('http');
-// // var server = http.createServer(function(request, response) {
-
-// //     response.writeHead(200, {"Content-Type": "text/plain"});
-// //     response.end("Hello World initial app!");
-
-// // });
-
-// var port = process.env.PORT || 1337;
-// app.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
+app.listen(port);
